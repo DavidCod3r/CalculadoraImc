3f607187833da1cf151cc70835cf23109d050e21
David Almeida <davidalmeidadrive@gmail.com>
2020-10-18 14:43:56 -0300

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@ form.addEventListener('submit', (event)=>{
     const peso = Number(document.querySelector("#peso").value);
     const altura = Number(document.querySelector("#altura").value);
 
+
     if(!peso){
         setResultado("Peso invalido", false);
         return;
@@ -19,6 +20,11 @@ form.addEventListener('submit', (event)=>{
     }
 
     const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é: ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
 });
 
 
@@ -27,23 +33,34 @@ function getNivelImc(imc){
 
     if(imc >= 39.9){
         return nivel[5];
-    }else if(nivel >= 34.9){
+    }
+    
+    if(imc >= 34.9){
         return nivel[4];
-    }else if(nivel >= 29.9){
+    }
+    
+    if(imc >= 29.9){
         return nivel[3];
-    }else if(nivel >= 24.9){
+    }
+    
+    if(imc >= 24.9){
         return nivel[2];
-    }else if(nivel >= 18.5){
+    }
+    
+    if(imc >= 18.5){
         return nivel[1];
-    }else if(imc < 18.5){
+    }
+    
+    if(imc < 18.5){
         return nivel[0];
     }
+    
 }
 
 
 function getImc(peso, altura){
     const imc = peso / (altura * altura);
-    imc.toFixed(2);
+    return imc.toFixed(2);
 }
 
 
@@ -58,6 +75,13 @@ function setResultado(msg, isValid){
     resultado.innerHTML = '';
 
     const p = criaP();
+    if(isValid){
+        p.classList.add('paragrafo-resultado');
+    }else{
+        p.classList.add('bad');
+
+    }
+
     p.innerHTML = msg;
     resultado.appendChild(p);
 }
